fbed9f276fdb8b069d5e19f4f2e802a418216f19
liankui <yuekewin@gmail.com>
2023-06-28 22:35:23 +0800

    feat: add lexer struct

---
+++ lexer/lexer_test.go
@@ -0,0 +1,41 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/chaos-io/go-verse/token"
+)
+
+func TestLexer_NextToken(t *testing.T) {
+	input := "=+,;(){}"
+	l := New(input)
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.EOF, ""},
+	}
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
--- token/token.go
+++ token/token.go
@@ -1,6 +1,31 @@
-// token/token.go
-
+// Package token, token/token.go
 package token
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// 标识符，字面量
+	IDENT = "IDENT" // add, foobar, x, y
+	INT   = "INT"   // 1234
+
+	// 运算符
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// 分隔符
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// 关键字
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
 
 type TokenType string
 
