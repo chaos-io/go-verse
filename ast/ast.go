85704cab06f890cea7c58c4db5bef7a7c440c13a
Eric Yue <yuekewin@gmail.com>
2023-09-02 22:35:08 +0800

    feat: 调用表达式 add(2, 3)

--- ast/ast.go
+++ ast/ast.go
@@ -60,7 +60,7 @@ func (fl *FunctionLiteral) String() string {
 
 	out.WriteString(fl.TokenLiteral())
 	out.WriteString("(")
-	out.WriteString(strings.Join(params, ","))
+	out.WriteString(strings.Join(params, ", "))
 	out.WriteString(")")
 	out.WriteString(fl.Body.String())
 
@@ -136,6 +136,30 @@ func (ie *IfExpression) String() string {
 		out.WriteString("else")
 		out.WriteString(ie.Alternative.String())
 	}
+
+	return out.String()
+}
+
+type CallExpression struct {
+	Token     token.Token // '(' 词法单元
+	Function  Expression  // 标识符或函数字面量
+	Arguments []Expression
+}
+
+func (ce *CallExpression) expressionNode()      {}
+func (ce *CallExpression) TokenLiteral() string { return ce.Token.Literal }
+func (ce *CallExpression) String() string {
+	var out bytes.Buffer
+
+	var args []string
+	for _, a := range ce.Arguments {
+		args = append(args, a.String())
+	}
+
+	out.WriteString(ce.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
 
 	return out.String()
 }
